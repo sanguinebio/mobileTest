23df7cc0cfa2aa4a305902757a75b0b6160acc76
Teja Rudraraju <rudrarajut@gmail.com>
2014-06-02 15:30:15 -0400

    Test app final commit

---
+++ app/scripts/controllers/form.js
@@ -0,0 +1,5 @@
+'use strict';
+
+sanguineTestApp.controller('formControl', function ($scope, $http) {
+  
+});
---
+++ app/scripts/controllers/splash.js
@@ -0,0 +1,7 @@
+'use strict';
+
+sanguineTestApp.controller('splashControl', function ($scope, $http) {
+
+    $scope.vidCaption = 'Bridging the gap between patients and researchers. Click the play button to learn more about us.';
+  
+  });
