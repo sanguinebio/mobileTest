b5f968388f8739a725f1b70814b130b3d02eb602
Teja Rudraraju <rudrarajut@gmail.com>
2014-06-03 18:37:30 -0400

    added build process

--- app/scripts/controllers/process.js
+++ app/scripts/controllers/process.js
@@ -7,25 +7,25 @@ sanguineTestApp.controller('processControl', function ($scope, $http, $interval)
         step: 1,
         title: 'step one: Sign Up',
         desc: 'Start advancing research by signing up online or calling for more information. A study coordinator will call you to explain the donation process, answer any questions and collect some basic health information.',
-        imgSrc: 'images/signup_image_1.png'
+        imgSrc: '/images/da1edc7b.signup_image_1.png'
       },
       {
         step: 2,
         title: 'step two: Schedule',
         desc: 'After you are screened, you will join our list of eligible donors! We will work to match you to a current research study. As soon as we do, we will call to schedule a convenient time for you to donate.',
-        imgSrc: 'images/schedule_image_1.png'
+        imgSrc: '/images/3a1768ec.schedule_image_1.png'
       },
       {
         step: 3,
         title: 'step three: Donate',
         desc: 'Your appointment takes place in the comfort of your home. During the appointment you will get your blood drawn and receive compensation. Our patient advocates (P.A.’s) are expert phlebotomists, dedicated researchers and true patient advocates.',
-        imgSrc: 'images/donate_image.png'
+        imgSrc: '/images/03e88a68.donate_image.png'
       },
       {
         step: 4,
         title: 'step four: Join',
         desc: 'It all starts with YOU!',
-        imgSrc: 'images/join_image_1.png'
+        imgSrc: '/images/79603d55.join_image_1.png'
       }
     ];
 
